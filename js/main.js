6e8a8c31ddd433fa1dcc962980fc4aa327e5e2f8
Barry <101702003@nccu.edu.tw>
2016-06-24 10:05:20 +0800

    A

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function deselect(selectItem, popoutItem) {
+  $(popoutItem).slideFadeToggle(function() {
+    selectItem.removeClass('selected');
+  });    
+}
+
+function popout(selectItem, popoutItem){
+ 	$(selectItem).on('click', function() {
+      $(this).addClass('selected');
+      $(popoutItem).slideFadeToggle();
+    return false;
+  	});	
+}
+
+function close(selectItem,closeItem,popoutItem){
+	$(closeItem).on('click', function() {
+		deselect($(selectItem),popoutItem);
+		return false;
+	});
+}
+
+//#1 breakfast introduce
+popout('#intro_link1','#f1');
+close('#intro_link1','#c1', '#f1');
+
+//#2 breakfast introduce
+popout('#intro_link2','#f2');
+close('#intro_link2','#c2', '#f2');
+
+//#3 breakfast introduce
+popout('#intro_link3','#f3');
+close('#intro_link3','#c3', '#f3');
+
+$.fn.slideFadeToggle = function(easing, callback) {
+  return this.animate({ opacity: 'toggle'}, 'fast', easing, callback);
+};
